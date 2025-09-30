25e49cf29f1b84cc6f39814c04c49352c292a257
Rida F'kih <hello@rida.dev>
2025-09-29 21:08:52 -0400

    Create autocompletion.zsh

---
+++ autocompletion.zsh
@@ -0,0 +1,57 @@
+autoload -Uz compinit; compinit
+autoload bashcompinit; bashcompinit
+autoload -U history-search-end
+
+eval "$(op completion zsh)"
+eval "$(docker completion zsh)"
+eval "$(gh completion -s zsh)"
+eval "$(uv generate-shell-completion zsh)"
+eval "$(pip completion --zsh)"
+eval "$(npm completion)"
+
+compdef _git git
+compdef _op op
+
+complete -C "$(which aws_completer)" aws
+
+HISTSIZE=10000
+SAVEHIST=10000
+
+zstyle ':completion:*' auto-description 'specify: %d'
+zstyle ':completion:*' completer _expand _complete _correct _approximate
+zstyle ':completion:*' format 'Completing %d'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' menu select=2
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+
+zstyle ':completion:*' menu select
+zstyle ':completion:*' rehash true
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*' use-compctl false
+zstyle ':completion:*' verbose true
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+
+zstyle ':omz:plugins:nvm' lazy yes
+
+setopt SHARE_HISTORY
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_FIND_NO_DUPS
+setopt HIST_SAVE_NO_DUPS
+setopt AUTO_CD
+setopt AUTO_LIST
+setopt AUTO_MENU
+setopt ALWAYS_TO_END
+
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^[[A" history-beginning-search-backward-end
+bindkey "^[[B" history-beginning-search-forward-end
+
+source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
